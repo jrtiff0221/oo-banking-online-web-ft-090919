f743b52ffb28dac24ad1da256923abb4adf88c16
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-26 18:38:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,6 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     end
   end
-end
   def reverse_transfer
     if @status == "complete"
       @sender.balance += @amount
