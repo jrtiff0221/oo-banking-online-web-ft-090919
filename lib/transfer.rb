eb424ef5372b790c102a0e70e7ea2528853a2946
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-26 18:18:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,8 @@ class Transfer
   attr_accessor :sender, :receiver, :status
   
   def initialize(sender, receiver, amount)
-    @transfer = transfer
+    @sender = sender
+    @receiver = receiver
     @status = 'pending'
   end
 
