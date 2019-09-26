48062a228b9fd79f8ad9b4da562b59505e90767a
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-26 18:41:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,12 +16,13 @@ class Transfer
       if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      
+      @status = "complete"
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
+  
   def reverse_transfer
     if @status == "complete"
       @sender.balance += @amount
